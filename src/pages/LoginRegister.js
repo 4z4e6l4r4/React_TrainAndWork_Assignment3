daeb89ace8a8fadaca2de2c86ed7c329fc1958d4
Azra <yukselazrael@gmail.com>
2023-12-31 02:09:24 +0300

    Updates

--- src/pages/LoginRegister.js
+++ src/pages/LoginRegister.js
@@ -1,31 +1,12 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+
 
 export default class LoginRegister extends Component {
   render() {
     return (
-      <div className="container">
-
-      <div className="col-xl-5 col-lg-4 ms-350">
-        <Link to={"/"}><button style={{background:"black",color:"white"}}>Home</button> </Link>
-        <ul class="nav nav-tabs " id="myTab" role="tablist">
-  <li class="nav-item" role="presentation">
-    <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home-tab-pane" type="button" role="tab" aria-controls="home-tab-pane" aria-selected="true">Giriş</button>
-  </li>
-  <li class="nav-item" role="presentation">
-    <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile-tab-pane" type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false">Kaydol</button>
-  </li>
-
-</ul>
-<div class="tab-content" id="myTabContent">
-  <div class="tab-pane fade show active" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab" tabindex="0">...</div>
-  <div class="tab-pane fade" id="profile-tab-pane" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">...</div>
-  <div class="tab-pane fade" id="contact-tab-pane" role="tabpanel" aria-labelledby="contact-tab" tabindex="0">...</div>
-  <div class="tab-pane fade" id="disabled-tab-pane" role="tabpanel" aria-labelledby="disabled-tab" tabindex="0">...</div>
-</div>
+      <div>
+      
       </div>
-      </div>
-
-    )
+    );
   }
 }
