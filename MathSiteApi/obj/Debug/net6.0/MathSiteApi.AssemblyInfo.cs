5500f6536e34e1380c05a3989da34718ef8e5df2
Azra <yukselazrael@gmail.com>
2024-01-13 23:44:28 +0300

    added Data

--- MathSiteApi/obj/Debug/net6.0/MathSiteApi.AssemblyInfo.cs
+++ MathSiteApi/obj/Debug/net6.0/MathSiteApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MathSiteApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5756221b842ce616b048e797b89aa054bc6c2582")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MathSiteApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MathSiteApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
